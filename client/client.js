f94a20cdeaaeaeb0f1bf6f350d492e5a28779496
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-03-11 19:03:35 +0900

    Add logs

--- client/client.js
+++ client/client.js
@@ -3,7 +3,13 @@ import io from "socket.io-client";
 const socket = io();
 
 socket.on("connect", () => {
+  console.log("connected");
+
   socket.on("content-changed", (content) => {
+    console.log("received content-changed events");
+
     __markdown_presentation_events__.emit("content-changed", content);
   });
+
+  console.log("listening content-changed events");
 });
