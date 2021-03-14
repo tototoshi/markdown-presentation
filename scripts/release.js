41b59b3f18414037d4134380c26adf2191122b07
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-03-15 08:30:09 +0900

    Refactor the release script

--- scripts/release.js
+++ scripts/release.js
@@ -4,29 +4,22 @@ const path = require("path");
 
 const child_process = require("child_process");
 const spawn = require("./util/spawn");
-const question = require("./util/question");
-const confirm = require("./util/confirm");
+const prompts = require("prompts");
 const writeFileUtf8 = require("write-file-utf8");
 
 const root = path.resolve(__dirname, "..");
 const packageJson = path.resolve(root, "package.json");
 const packageInfo = require(path.resolve(root, "package.json"));
 
-async function main() {
-  process.chdir(root);
-
+function checkIfWorkingDirectoryIsClean() {
   const diff = child_process.execSync("git status --porcelain");
   if (diff.length > 0) {
     console.error("Workspace is not clean");
     process.exit(1);
   }
+}
 
-  const version = await question(
-    `Please input the next version (current: ${packageInfo.version}): `
-  );
-
-  console.log(`Will release version ${version}`);
-
+async function updatePackgeJson(version) {
   await writeFileUtf8(
     packageJson,
     JSON.stringify({ ...packageInfo, version: version }, null, 2),
@@ -34,27 +27,73 @@ async function main() {
   );
 
   await spawn("npm", ["install"]);
+}
 
-  if (!(await confirm("Do you want to commit? [y/n]: "))) {
+async function commit(version) {
+  const answer = await prompts({
+    type: "confirm",
+    message: "Do you want to commit?",
+    name: "doCommit",
+  });
+
+  if (!answer.doCommit) {
     return;
   }
 
   await spawn("git", ["add", "package.json", "package-lock.json"]);
   await spawn("git", ["commit", "-m", `v${version}`]);
   await spawn("git", ["tag", `v${version}`]);
+}
 
-  if (!(await confirm("Do you want to publish? [y/n]: "))) {
+async function publish() {
+  const answer = await prompts({
+    type: "confirm",
+    message: "Do you want to publish?",
+    name: "doPublish",
+  });
+
+  if (!answer.doPublish) {
     return;
   }
 
   await spawn("npm", ["publish", "--access", "public"]);
+}
 
-  if (!(await confirm("Do you want to push commits and tags? [y/n]: "))) {
+async function pushCommitAndTag() {
+  const answer = await prompts({
+    type: "confirm",
+    message: "Do you want to push commits and tags?",
+    name: "doPush",
+  });
+
+  if (!answer.doPush) {
     return;
   }
 
   await spawn("git", ["push"]);
   await spawn("git", ["push", "--tags"]);
+}
+
+async function main() {
+  process.chdir(root);
+
+  checkIfWorkingDirectoryIsClean();
+
+  const answer = await prompts({
+    type: "text",
+    message: `Please input the next version (current: ${packageInfo.version})`,
+    name: "version",
+  });
+
+  console.log(`Will release version ${answer.version}`);
+
+  await updatePackgeJson(answer.version);
+
+  await commit(answer.version);
+
+  await publish();
+
+  await pushCommitAndTag();
 
   console.log("All Done!");
 }
--- scripts/util/confirm.js
+++
@@ -1,5 +0,0 @@
-const question = require("./question");
-
-module.exports = function confirm(message) {
-  return question(message).then((value) => /^[yY]$/.test(value));
-};
